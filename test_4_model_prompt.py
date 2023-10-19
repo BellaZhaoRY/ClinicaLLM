dce33e0b60b56ff5bf969ed6888f9ba9c78c0534
Yuxin He <53741137+Stardust-hyx@users.noreply.github.com>
2023-10-19 21:48:46 +0800

    Update test_4_model_prompt.py

--- test_4_model_prompt.py
+++ test_4_model_prompt.py
@@ -5,12 +5,12 @@ import json
 import os
 from config.config import *
 import pandas as pd
-# from transformers import AutoTokenizer, AutoModel
-os.environ['CUDA_VISIBLE_DEVICES'] = '1,2,3'
+from transformers import AutoTokenizer, AutoModel
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
-# my_model_path = os.path.join(model_dir_path,"")
-# tokenizer = AutoTokenizer.from_pretrained(model_dir_path+"/chatglm2-6b", trust_remote_code=True)
-# model = AutoModel.from_pretrained(model_dir_path, trust_remote_code=True).cuda()
+my_model_path = "/home/ubuntu/hyx/LLaMA-Efficient-Tuning-main/HYX_ChatGLM2_"
+tokenizer = AutoTokenizer.from_pretrained(my_model_path, trust_remote_code=True)
+model = AutoModel.from_pretrained(my_model_path, trust_remote_code=True).cuda()
 
 # tokenizer = AutoTokenizer.from_pretrained("THUDM/ChatGLM2-6B", trust_remote_code=True)
 # model = AutoModel.from_pretrained("THUDM/ChatGLM2-6B", trust_remote_code=True,cache_dir=model_dir_path).cuda()
@@ -19,27 +19,28 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '1,2,3'
 # from huggingface_hub import snapshot_download
 # snapshot_download(repo_id="THUDM/chatglm-6b", local_dir=model_dir_path+"/chatglm-6b/")
 
-from modelscope.utils.constant import Tasks  
-from modelscope import Model
-from modelscope.pipelines import pipeline
+# from modelscope.utils.constant import Tasks  
+# from modelscope import Model
+# from modelscope.pipelines import pipeline
 # DEVICE = "cuda"
 # DEVICE_ID = ""
 # CUDA_DEVICE = f"{DEVICE}:{DEVICE_ID}" if DEVICE_ID else DEVICE
-model = Model.from_pretrained('ZhipuAI/chatglm2-6b', device_map='auto', revision='v1.0.12')
+# model = Model.from_pretrained('ZhipuAI/chatglm2-6b', device_map='auto', revision='v1.0.12')
 # model = Model.from_pretrained('ZhipuAI/chatglm2-6b', revision='v1.0.7',max_length=31*1024,do_sample=False,device_map='auto')
 # model = Model.from_pretrained('ZhipuAI/chatglm2-6b', device_map=CUDA_DEVICE, revision='v1.0.7',max_length=31*1024,do_sample=False)
 
-pipe = pipeline(task=Tasks.chat, model=model,topk=0.000000001,temperature=0.000000001)
-inputs = {"text": prompt, "history": history}
-result = pipe(inputs)
+# pipe = pipeline(task=Tasks.chat, model=model,topk=0.000000001,temperature=0.000000001)
+# inputs = {"text": prompt, "history": history}
+# result = pipe(inputs)
 
 def get_result_4_re(rule_info, context_4_stem_vid):
     # 基于正则的方式获取结果
     res_dict = {}
+    # print(rule_info)
     res = re.finditer(rule_info, context_4_stem_vid)
     if res:
         for x in res:
-            for group_name,group_value in  x.groupdict():
+            for group_name,group_value in  x.groupdict().items():
                 if group_value:
                     res_dict[group_name] = group_value
     else:
@@ -53,14 +54,39 @@ def get_result_4_model(file_re_info, sec_index_re,stem_cn_name,rule_info,context
     :param file_re_info: 文件名信息，即一级索引
     :param sec_index_re: 表索引信息，即二级信息
     :param stem_cn_name: stem中文名
-    :param rule_info: stem中补充的医学知识
+    :param rule_info: prompt
     :param context_4_stem_vid: 该就诊中关于该stem的相关病历文本信息
     :return:
     """
-    prompt = f"你将获得一份在{file_re_info}中关于{sec_index_re}的相关病历，请你根据‘’‘{rule_info}’‘’中的医学知识获得‘’‘{stem_cn_name}’‘’问题的答案，\n病历：{context_4_stem_vid}。因此{stem_cn_name}的答案为"
-    inputs = {"text": prompt, "history": []}
-    result = pipe(inputs)
+    rule_info = rule_info.replace("\\n", "\n")
+    prompt = f"你将阅读一段来自{file_re_info}的病历文本，并根据病历内容回答一个问题。\n病历文本：\n{context_4_stem_vid}\n根据病历内容，请问{rule_info}"
+    r, h = model.chat(tokenizer, prompt, history=[], do_sample=False)
+    print("*"*100)
+    print(prompt)
+    print()
+    print(r)
+    print("*"*100)
+    result = parse_model_answer(r, stem_cn_name)
     return result
+def parse_model_answer(response, term):
+    # 对模型回复进行解析得到答案
+    # if response in ['是', '否']:
+    #     return yes_or_no_mapping[response]
+
+    res_dict = {}
+    mapping_rule = "(?P<y>是)|(?P<n>否)|(?P<a>A\.)|(?P<b>B\.)|(?P<c>C\.)|(?P<d>D\.)|(?P<UTD>E\.)"
+    res = re.finditer(mapping_rule, response)
+    if res:
+        for x in res:
+            for group_name,group_value in  x.groupdict().items():
+                if group_value:
+                    res_dict[group_name] = group_value
+    else:
+        print(response)
+        exit(0)
+    res_group_name = [re.sub("\d+","",x) for x in list(res_dict.keys()) if x]
+    res_group_name = sorted(list(set(res_group_name)))
+    return "\\".join(res_group_name)
 def check_stem_res_and_type(stem_type, stem_res):
     if stem_type == "字符串":
         assert len(stem_res.split("\\")) == 1
@@ -71,28 +97,71 @@ def check_stem_res_and_type(stem_type, stem_res):
 def combine_stem_res_4_rule_and_model(stem_results):
     # stem_results：[{规则：答案},{模型：答案}]
     # 若既有规则又有模型，获取答案的依据是以规则为准==》若规则答案冲突，则以y为准 todo
-    all_res = list(set([ans for xdict in stem_results for rl,ans in xdict.items()]))
-    rule_res = list(set([ans for xdict in stem_results for rl,ans in xdict.items() if rl == "规则"]))
-    # model_res = list(set([ans for xdict in stem_results for rl,ans in xdict.items() if rl == "模型"]))
+    all_res = list(set([ans for xdict in stem_results for rl,ans in xdict.items() if ans]))
+    rule_res = list(set([ans for xdict in stem_results for rl,ans in xdict.items() if rl == "规则" and ans]))
+    model_res = list(set([ans for xdict in stem_results for rl,ans in xdict.items() if rl == "模型" and ans]))
 
     if len(all_res) == 1:
         return all_res[0]
+    elif len(rule_res) == 1:
+        return rule_res[0]
+    elif len(model_res) == 1:
+        return model_res[0]
     else:
-        if len(rule_res) == 1:
-            return rule_res[0]
+        if "y" in rule_res:
+            return "y"
         else:
-            if "y" in rule_res:
-                return "y"
-            else:
-                return "n"
-def get_context_info_4_vid_4_stem(file_re_info,sec_index_re,cli_info_4_vid):
+            return "n"
+
+def get_precondition_2_secect_line(stem_cn_name,condition_info):
+    """
+
+    :param stem_cn_name:
+    :param condition_info:
+    :return:
+    """
+    if re.search("首",stem_cn_name):
+        return condition_info.get("首次入院时间","")
+    elif re.search("围术期",stem_cn_name):
+        terms = condition_info.get("围术期",[])
+        return terms
+        # for term in terms:
+        #     for start_time,end_time in term:
+        #         start_time = int(start_time) -1
+        #         end_time = int(end_time) +1
+        #         return
+    else:
+        return ""
+def is_filter_4_sec_index(precondition_re,sec_index):
+    if isinstance(precondition_re, str):
+        if re.search(precondition_re, sec_index):
+            return False
+    else:
+        for pre_re in precondition_re:
+            for start_time, end_time in pre_re:
+                if start_time <= int(sec_index[-2]) <= end_time:
+                    return False
+    return True
+def get_context_info_4_vid_4_stem(file_re_info,sec_index_re,cli_info_4_vid,line_re,stem_cn_name):
+    # stem_cn_name: 如果是“首”相关，则医嘱规则增加 首次入院时间和sec_index中医嘱开始相同相同；
+    #                 如果是“围术期”,则医嘱规则增加 医嘱开始和结束时间限制下 sec_index筛选条件
     context = ""
-    context_format = "该病历%s中关于%s的信息为：%s"
+    # 获取前置条件
+    precondition_re = get_precondition_2_secect_line(stem_cn_name,cli_info_4_vid.get("补充信息",{}))
     for file_name, cli_info in cli_info_4_vid.items():
         if re.search(file_re_info, file_name):
             for sec_index, sec_info in cli_info.items():
+                if precondition_re:
+                    if is_filter_4_sec_index(precondition_re, sec_index):
+                       continue
+                sec_index = re.sub("_\d{4,}","",sec_index)  # 删除掉时间信息
                 if re.search(sec_index_re, sec_index):
-                    context += context_format % (file_name, sec_index, sec_info)
+                    # 通过line_re行筛选依据，过滤无用信息
+                    if isinstance(sec_info,list):
+                        line_str = "\n".join([x for x in sec_info if re.search(line_re,x)])
+                    else:
+                        line_str = sec_info if re.search(line_re,sec_info) else ""
+                    context += line_str + '\n'
     return context
 def get_all_stem_info():
     result_dict_path = os.path.join(prepro_orig_data_dir_path, "result_dict.json")
@@ -100,10 +169,7 @@ def get_all_stem_info():
         stem_info_dict = json.load(f)
     new_stem_info_dict = {}
     for stem_name,stem_info in stem_info_dict.items():
-        if not re.search("STEMI-[456]|STEMI-3-2-7",stem_name):  # todo
-            continue
-        else:
-            new_stem_info_dict[stem_name] = stem_info
+        new_stem_info_dict[stem_name] = stem_info
     return new_stem_info_dict
 def get_check_vids_info():
     # 读取就诊列表中的就诊id信息，并核对和解析的数据中vid是否相同
@@ -113,11 +179,11 @@ def get_check_vids_info():
     prepro_vids = os.listdir(prepro_data_dir_path)
     assert len(all_vids) == len(prepro_vids)
     return all_vids
-def get_cli_info_4_vid(files_4_vid):
+def get_cli_info_4_vid(vid_file_path,files_4_vid):
     # 读取该就诊下的所有病历信息
     cli_info_4_vid = {}
     for file_4_cli_info in files_4_vid:
-        with open(os.path.join(prepro_data_dir_path, file_4_cli_info), "r", encoding="utf-8") as f:
+        with open(os.path.join(vid_file_path, file_4_cli_info), "r", encoding="utf-8") as f:
             cli_info = json.load(f)
         cli_info_4_vid[file_4_cli_info[:-5]] = cli_info
     return cli_info_4_vid
@@ -131,7 +197,7 @@ def main():
         vid_file_path = os.path.join(prepro_data_dir_path,vid)
         files_4_vid = os.listdir(vid_file_path)
         # 3.1 加载该就诊下的所有转化格式后的病历信息
-        cli_info_4_vid = get_cli_info_4_vid(files_4_vid)
+        cli_info_4_vid = get_cli_info_4_vid(vid_file_path,files_4_vid)
         # 3.2 依次获得每个stem的结果，需要根据有向无环图的顺序获取结果
         for stem_name,stem_info in new_stem_info_dict.items():
             stem_cn_name = stem_info["数据采集项"]
@@ -145,10 +211,11 @@ def main():
                 if isinstance(stem_info,dict):
                     file_re_info = stem_info["文件名"].strip()
                     sec_index_re = stem_info["表索引"].strip()
+                    line_re = stem_info["行筛选条件"].strip()
                     parser_fun = stem_info["解析方式"].strip()
                     rule_info = stem_info["规则"].strip()
                     # 3.4 获取该就诊关于该stem的相关病历内容。
-                    context_4_stem_vid = get_context_info_4_vid_4_stem(file_re_info, sec_index_re, cli_info_4_vid)
+                    context_4_stem_vid = get_context_info_4_vid_4_stem(file_re_info, sec_index_re, cli_info_4_vid,line_re,stem_cn_name)
                     # 3.5 根据规则或模型，获取相应答案，并使答案符合比赛要求
                     if parser_fun == "规则":
                         stem_res_4_rule = get_result_4_re(rule_info,context_4_stem_vid)
@@ -156,6 +223,7 @@ def main():
                     elif parser_fun == "模型":
                         stem_res_4_model = get_result_4_model(file_re_info, sec_index_re,stem_cn_name,rule_info, context_4_stem_vid)
                         stem_results.append({"模型":stem_res_4_model})
+                        print(f"模型预测答案为{stem_res_4_model}\n")
                     else:
                         raise print(f"{stem_name}\t{stem_cn_name}\t的解析方式为{parser_fun},错误.")
                 else:
@@ -166,7 +234,8 @@ def main():
                     # 3.7 数据类型为字符串的答案结果应该只有一个，因此需要核对结果
                     check_stem_res_and_type(stem_type,stem_res)
                 except:
-                    raise print(f"{vid}就诊中{stem_name}:{stem_cn_name}的数据类型为{stem_type}，答案为{stem_res},不符合要求。")
+                    # raise \
+                    print(f"{vid}就诊中{stem_name}:{stem_cn_name}的数据类型为{stem_type}，规则为{stem_info}，预测答案为{stem_res},不符合要求。\n")
 
 if __name__ == '__main__':
     main()
